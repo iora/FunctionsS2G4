706868595c9c24a692b663e98e364d99b8785ea5
annanurov <annanurov@gmail.com>
2017-04-28 11:16:48 +0700

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*	
+*	Please define your functions after main().
+*	You can import standard C / C++ libraries.
+*	Recommended: add description of your code.
+*/
+
+int getSum(int myArray[]);
+int getSumOfSquares(int myArray[]);
+void printAsChars(int myArray[]);
+double getAverage(int myArray[]);
+int subtractAllValues(int myArray[]);
+float invertAllValues(int myArray[] );
+void printGroupMemberNames();
+
+int main(void) {
+	printGroupMemberNames();
+
+	int row[500];
+	for (int i = 0; i < 500; i++)
+		row[i] = i;
+
+	return 1;
+}
+
+
+
+
+void printGroupMemberNames() {
+	printf("Name1\n ");
+	printf("Name2\n ");
+	printf("Name3\n ");
+	printf("That's it!\n");
+}
+
